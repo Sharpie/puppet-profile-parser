942d480b67680259f02f20c51a3bd28a7e0aa00e
Charlie Sharpsteen <source@sharpsteen.net>
2018-02-25 13:26:07 -0800

    Re-organize main functions into a CLI class
    This commit takes the remaining methods defined on the PuppetProfiler module
    and re-organizes them into a PuppetProfiler::CLI class. This makes testing
    easier.

--- profile-parser.rb
+++ profile-parser.rb
@@ -136,65 +136,67 @@ module PuppetProfiler
     end
   end
 
-  def self.parse(line)
-    case line
-    when /Called/
-      FunctionSlice.new.parse(line)
-    when /Evaluated resource/
-      ResourceSlice.new.parse(line)
-    else
-      OtherSlice.new.parse(line)
-    end
-  end
-
-  def self.process_group(title, slices)
-    total = 0.0
-    itemized_totals = Hash.new { |h, k| h[k] = 0.0 }
-
-    slices.each do |slice|
-      total += slice.time
-      itemized_totals[slice.name] += slice.time
-    end
-
-    rows = itemized_totals.to_a.sort { |a, b| b[1] <=> a[1] }
-
-    table = Terminal::Table.new(:headings => ["Source", "Time"], :rows => rows)
-
-    puts "--- #{title} ---"
-    puts "Total time: #{total}"
-    puts "Itemized:"
-    puts table
-  end
-
-  def self.run
-    things = []
-
-    ARGF.each_line do |line|
-      next unless line.match(/PROFILE/)
-
-      # Strip off leader
-      if (match = line.match(/(PROFILE.*)$/))
-        things << parse(match[1])
+  class CLI
+    def parse(line)
+      case line
+      when /Called/
+        FunctionSlice.new.parse(line)
+      when /Evaluated resource/
+        ResourceSlice.new.parse(line)
+      else
+        OtherSlice.new.parse(line)
       end
     end
 
-    root = Namespace.new('1', nil)
+    def process_group(title, slices)
+      total = 0.0
+      itemized_totals = Hash.new { |h, k| h[k] = 0.0 }
 
-    things.each do |thing|
-      root.add(thing.id, thing)
+      slices.each do |slice|
+        total += slice.time
+        itemized_totals[slice.name] += slice.time
+      end
+
+      rows = itemized_totals.to_a.sort { |a, b| b[1] <=> a[1] }
+
+      table = Terminal::Table.new(:headings => ["Source", "Time"], :rows => rows)
+
+      puts "--- #{title} ---"
+      puts "Total time: #{total}"
+      puts "Itemized:"
+      puts table
     end
 
-    root.display
+    def run
+      things = []
 
-    funcalls = things.select { |thing| thing.is_a? FunctionSlice }
-    resevals = things.select { |thing| thing.is_a? ResourceSlice }
+      ARGF.each_line do |line|
+        next unless line.match(/PROFILE/)
 
-    process_group("Function calls", funcalls)
-    process_group("Resource evaluations", resevals)
+        # Strip off leader
+        if (match = line.match(/(PROFILE.*)$/))
+          things << parse(match[1])
+        end
+      end
+
+      root = Namespace.new('1', nil)
+
+      things.each do |thing|
+        root.add(thing.id, thing)
+      end
+
+      root.display
+
+      funcalls = things.select { |thing| thing.is_a? FunctionSlice }
+      resevals = things.select { |thing| thing.is_a? ResourceSlice }
+
+      process_group("Function calls", funcalls)
+      process_group("Resource evaluations", resevals)
+    end
   end
 end
 
 
 if __FILE__ == File.expand_path($PROGRAM_NAME)
-  PuppetProfiler.run
+  PuppetProfiler::CLI.new.run
 end
