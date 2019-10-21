ac121494b324d751138a2fe36b5e9e9b03b1e0ae
Dimitri Tischenko <dimitri@puppet.com>
2019-10-21 12:14:10 +0200

    now evaluating glob pattern to list analyzed logs

--- tasks/show_summary.sh
+++ tasks/show_summary.sh
@@ -3,8 +3,9 @@
 LOGS=${PT_logs:-/var/log/puppetlabs/puppetserver/puppetserver.log}
 RUBY=$(which ruby||echo "/opt/puppetlabs/puppet/bin/ruby")
 
-echo "Analyzing logs: $LOGS"
-# we allog $LOGS to contain globbing characters
+echo "Analyzing logs: "
+eval echo $LOGS
+# we allow $LOGS to contain globbing characters
 # FIXME: potential security issue with allowing any logfile to be specified
 # Probably need to restrict to files inside /var/log/puppetlabs/puppetserver
 $RUBY ${PT__installdir}/puppet_profile_parser/files/puppet-profile-parser.rb ${LOGS}
